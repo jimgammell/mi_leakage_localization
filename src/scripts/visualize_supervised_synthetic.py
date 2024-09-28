c8d9b652ca5d30cf05a30b9c8cbe1b74dd54cdfe
Jimmy Gammell <jgammell@purdue.edu>
2024-09-28 15:14:10 -0400

    Some more tweaks to the visualization code

--- src/scripts/visualize_supervised_synthetic.py
+++ src/scripts/visualize_supervised_synthetic.py
@@ -7,11 +7,13 @@ from collections import OrderedDict
 from _common import *
 
 Results = OrderedDict([
-    ('synthetic_boolean_masking_supervised_htune__preliminary', None),
-    ('synthetic_no_ops_supervised_htune__preliminary', None),
-    ('synthetic_shuffling_supervised_htune__preliminary', None),
-    ('synthetic_unprotected_supervised_htune__preliminary', None)
+    ('synthetic_boolean_masking_supervised_htune', None),
+    ('synthetic_no_ops_supervised_htune', None),
+    ('synthetic_shuffling_supervised_htune', None),
+    ('synthetic_unprotected_supervised_htune', None)
 ])
+CURVES_PLOT_COUNT = 5
+
 for sweep_dir in Results.keys():
     trial_dirs = os.listdir(os.path.join(OUTPUT_DIR, sweep_dir))
     results = {
@@ -26,10 +28,13 @@ for sweep_dir in Results.keys():
         if not 'learning_rate=' in trial_dir:
             continue
         learning_rate = float(trial_dir.split('learning_rate=')[-1])
-        with open(os.path.join(OUTPUT_DIR, sweep_dir, trial_dir, 'training_curves.pickle'), 'rb') as f:
-            training_curves = pickle.load(f)
-        with open(os.path.join(OUTPUT_DIR, sweep_dir, trial_dir, 'rank_over_time.pickle'), 'rb') as f:
-            rank_over_time = pickle.load(f)
+        try:
+            with open(os.path.join(OUTPUT_DIR, sweep_dir, trial_dir, 'training_curves.pickle'), 'rb') as f:
+                training_curves = pickle.load(f)
+            with open(os.path.join(OUTPUT_DIR, sweep_dir, trial_dir, 'rank_over_time.pickle'), 'rb') as f:
+                rank_over_time = pickle.load(f)
+        except:
+            continue
         results['learning_rate'].append(learning_rate)
         results['train_loss'].append(training_curves['train-loss'])
         results['val_loss'].append(training_curves['val-loss'])
@@ -45,27 +50,28 @@ for sweep_dir in Results.keys():
 
 for dir, results in Results.items():
     trial_count = len(results['learning_rate'])
-    fig, axes = plt.subplots(3, trial_count, figsize=(2*trial_count, 2*3))
-    for trial_idx in range(trial_count):
-        axes[0, trial_idx].plot(results['train_loss'][trial_idx, 0, :], results['train_loss'][trial_idx, 1, :], color='red', linestyle='--', rasterized=True)
-        axes[0, trial_idx].plot(results['val_loss'][trial_idx, 0, :], results['val_loss'][trial_idx, 1, :], color='red', linestyle='-', rasterized=True)
-        axes[1, trial_idx].plot(results['train_rank'][trial_idx, 0, :], results['train_rank'][trial_idx, 1, :], color='red', linestyle='--', rasterized=True)
-        axes[1, trial_idx].plot(results['val_rank'][trial_idx, 0, :], results['val_rank'][trial_idx, 1, :], color='red', linestyle='-', rasterized=True)
+    fig, axes = plt.subplots(3, CURVES_PLOT_COUNT, figsize=(12, 12*3/CURVES_PLOT_COUNT))
+    trial_indices = np.linspace(0, trial_count-1, CURVES_PLOT_COUNT).astype(int)
+    for ax_idx, trial_idx in enumerate(trial_indices):
+        axes[0, ax_idx].plot(results['train_loss'][trial_idx, 0, :], results['train_loss'][trial_idx, 1, :], color='red', linestyle='--', rasterized=True)
+        axes[0, ax_idx].plot(results['val_loss'][trial_idx, 0, :], results['val_loss'][trial_idx, 1, :], color='red', linestyle='-', rasterized=True)
+        axes[1, ax_idx].plot(results['train_rank'][trial_idx, 0, :], results['train_rank'][trial_idx, 1, :], color='red', linestyle='--', rasterized=True)
+        axes[1, ax_idx].plot(results['val_rank'][trial_idx, 0, :], results['val_rank'][trial_idx, 1, :], color='red', linestyle='-', rasterized=True)
         rank_over_time = results['rank_over_time'][trial_idx]
-        axes[2, trial_idx].fill_between(np.arange(1, rank_over_time.shape[1]+1), np.percentile(rank_over_time, 25, axis=0), np.percentile(rank_over_time, 75, axis=0), color='blue', alpha=0.25, rasterized=True)
-        axes[2, trial_idx].plot(np.arange(1, rank_over_time.shape[1]+1), np.median(rank_over_time, axis=0), color='blue', linestyle='-', rasterized=True)
-        axes[0, trial_idx].set_title(f'lr={results["learning_rate"][trial_idx]:.1e}')
-        axes[2, trial_idx].set_xlabel('Traces seen')
-        axes[2, trial_idx].set_ylabel('Correct-key rank')
-        axes[1, trial_idx].set_xlabel('Training steps')
-        axes[0, trial_idx].set_xlabel('Training steps')
-        axes[0, trial_idx].set_ylabel('Loss')
-        axes[1, trial_idx].set_ylabel('Mean rank')
-        axes[2, trial_idx].set_xscale('log')
+        axes[2, ax_idx].fill_between(np.arange(1, rank_over_time.shape[1]+1), np.percentile(rank_over_time, 25, axis=0), np.percentile(rank_over_time, 75, axis=0), color='blue', alpha=0.25, rasterized=True)
+        axes[2, ax_idx].plot(np.arange(1, rank_over_time.shape[1]+1), np.median(rank_over_time, axis=0), color='blue', linestyle='-', rasterized=True)
+        axes[0, ax_idx].set_title(f'lr={results["learning_rate"][trial_idx]:.1e}')
+        axes[2, ax_idx].set_xlabel('Traces seen')
+        axes[2, ax_idx].set_ylabel('Correct-key rank')
+        axes[1, ax_idx].set_xlabel('Training steps')
+        axes[0, ax_idx].set_xlabel('Training steps')
+        axes[0, ax_idx].set_ylabel('Loss')
+        axes[1, ax_idx].set_ylabel('Mean rank')
+        axes[2, ax_idx].set_xscale('log')
     fig.tight_layout()
     fig.savefig(os.path.join(get_trial_dir(), f'{dir}__raw_output.pdf'))
     
-    fig, axes = plt.subplots(1, 3, figsize=(8, 4))
+    fig, axes = plt.subplots(1, 3, figsize=(12, 4))
     axes[0].plot(results['learning_rate'], results['val_loss'][:, 1, -1], color='blue', marker='.', linestyle='-', rasterized=True)
     axes[1].plot(results['learning_rate'], results['val_rank'][:, 1, -1], color='blue', marker='.', linestyle='-', rasterized=True)
     axes[2].plot(results['learning_rate'], np.mean(results['rank_over_time'], axis=(1, 2)), color='blue', marker='.', linestyle='-', rasterized=True)
@@ -75,5 +81,6 @@ for dir, results in Results.items():
     axes[0].set_ylabel('Final loss')
     axes[1].set_ylabel('Final mean rank')
     axes[2].set_ylabel('Final mean accumulated rank')
+    axes[0].set_ylim(5, 10)
     fig.tight_layout()
     fig.savefig(os.path.join(get_trial_dir(), f'{dir}__sweep.pdf'))
